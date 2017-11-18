160c4afc36578ca8b26c3794bb483526e7a051a8
severedsolo <severedsolo@users.noreply.github.com>
2017-11-18 07:16:35 +0000

    Initial commit

---
+++ CraftLoader.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using UnityEngine;
+
+namespace CraftSaver
+{
+    [KSPScenario(ScenarioCreationOptions.AddToAllGames, GameScenes.EDITOR,GameScenes.SPACECENTER)]
+    class CraftLoader : ScenarioModule
+    {
+        string settingsLocation = KSPUtil.ApplicationRootPath + "/GameData/CraftSaver/PluginData/Settings.cfg";
+        public override void OnLoad(ConfigNode persistent)
+        {
+            ConfigNode[] crafts = persistent.GetNodes("CRAFT");
+            if(crafts.Count() >0)
+            {
+                CraftSaver.instance.alwaysSave.Clear();
+                for(int i = 0; i<crafts.Count();i++)
+                {
+                    ConfigNode node = crafts.ElementAt(i);
+                    CraftSaver.instance.alwaysSave.Add(node.GetValue("name"));
+                }
+            }
+            if(File.Exists(settingsLocation))
+            {
+                CraftSaver.instance.savedPath = File.ReadAllText(settingsLocation);
+            }
+            else
+            {
+                CraftSaver.instance.savedPath = KSPUtil.ApplicationRootPath + "/GameData/CraftSaver/PluginData/SavedCraft/";
+            }
+            string VABString = CraftSaver.instance.savedPath + "/VAB";
+            string SPHString = CraftSaver.instance.savedPath + "/SPH";
+            List<string> VABList = new List<string>();
+            if (Directory.Exists(VABString)) VABList = Directory.GetFiles(VABString).ToList();
+            List<string> SPHList = new List<string>();
+            if (Directory.Exists(SPHString)) SPHList = Directory.GetFiles(SPHString).ToList();
+            if (VABList.Count() >0)
+            {
+                for(int i = 0; i<VABList.Count(); i++)
+                {
+                    string s = VABList.ElementAt(i);
+                    string substring = s.Substring(VABString.Length + 1);
+                    if (File.Exists(CraftSaver.instance.VABPath + substring))
+                    {
+                        if (CraftSaver.instance.alwaysSave.Contains(substring)) File.Copy(CraftSaver.instance.VABPath + substring, s, true);
+                    }
+                    if (!File.Exists(CraftSaver.instance.VABPath + substring))
+                    {
+                        File.Copy(s, CraftSaver.instance.VABPath + substring);
+                        Debug.Log("[CraftSaver] Restored backup copy of " + s);
+                    }
+                }
+            }
+            if (SPHList.Count() > 0)
+            {
+                for (int i = 0; i < SPHList.Count(); i++)
+                {
+                    string s = SPHList.ElementAt(i);
+                    string substring = s.Substring(VABString.Length + 1);
+                    if (File.Exists(CraftSaver.instance.SPHPath + substring))
+                    {
+                        if (CraftSaver.instance.alwaysSave.Contains(substring)) File.Copy(CraftSaver.instance.SPHPath + substring, s, true);
+                    }
+                    if (!File.Exists(CraftSaver.instance.SPHPath + substring))
+                    {
+                        File.Copy(s, CraftSaver.instance.SPHPath + substring);
+                        Debug.Log("[CraftSaver]: Restored backup copy of " + s);
+                    }
+                }
+            }
+        }
+        public override void OnSave(ConfigNode persistent)
+        {
+            File.WriteAllText(settingsLocation, CraftSaver.instance.savedPath);
+            if (CraftSaver.instance.alwaysSave.Count() == 0) return;
+            for(int i = 0; i<CraftSaver.instance.alwaysSave.Count(); i++)
+            {
+                ConfigNode node = new ConfigNode("CRAFT");
+                node.SetValue("name", CraftSaver.instance.alwaysSave.ElementAt(i), true);
+                persistent.AddNode(node);
+            }
+        }
+    }
+}
---
+++ CraftSaver.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP.UI.Screens;
+using System.IO;
+
+namespace CraftSaver
+{
+    [KSPAddon(KSPAddon.Startup.SpaceCentre, true)]
+    public class CraftSaver : MonoBehaviour
+    {
+        public static CraftSaver instance;
+        public string savedPath = KSPUtil.ApplicationRootPath + "/GameData/CraftSaver/PluginData/SavedCraft/";
+        public string VABPath = KSPUtil.ApplicationRootPath + "/saves/" + HighLogic.SaveFolder + "/Ships/VAB/";
+        public string SPHPath = KSPUtil.ApplicationRootPath + "/saves/" + HighLogic.SaveFolder + "/Ships/SPH/";
+        public List<string> alwaysSave = new List<string>();
+        bool alwaysSaveToggle = false;
+        bool showGUI = false;
+        bool askToConfirm = false;
+        bool confirmed = false;
+        bool promptToSave = false;
+        ApplicationLauncherButton ToolbarButton;
+        Rect Window = new Rect(20, 100, 480, 50);
+        bool showCrafts = false;
+        Vector2 scrollPosition1;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+            instance = this;
+        }
+
+        private void Start()
+        {
+            GameEvents.onGUIApplicationLauncherReady.Add(GUIReady);
+            GameEvents.onGameSceneSwitchRequested.Add(SceneSwitch);
+        }
+
+        private void SceneSwitch(GameEvents.FromToAction<GameScenes, GameScenes> data)
+        {
+            showGUI = false;
+        }
+
+        private void GUIReady()
+        {
+            confirmed = false;
+            if (HighLogic.LoadedScene == GameScenes.MAINMENU) return;
+            if (ToolbarButton == null)
+            {
+                ToolbarButton = ApplicationLauncher.Instance.AddModApplication(GUISwitch, GUISwitch, null, null, null, null, ApplicationLauncher.AppScenes.VAB | ApplicationLauncher.AppScenes.SPH, GameDatabase.Instance.GetTexture("CraftSaver/Icon", false));
+            }
+        }
+
+        void GUISwitch()
+        {
+            showGUI = !showGUI;
+        }
+
+        private void OnGUI()
+        {
+            if (!showGUI) return;
+            Window = GUILayout.Window(55901837, Window, GUIDisplay, "CraftSaver", GUILayout.Width(480));
+        }
+        void GUIDisplay(int windowID)
+        { 
+            GUILayout.Label("Saved Craft Directory");
+            savedPath = GUILayout.TextField(savedPath);
+            if (GUILayout.Button("View Saved Crafts")) showCrafts = !showCrafts;
+            if(showCrafts)
+            {
+                scrollPosition1 = GUILayout.BeginScrollView(scrollPosition1, GUILayout.Width(480), GUILayout.Height(200));
+                List<string> vabCrafts = new List<string>();
+                List<string> sphCrafts = new List<string>();
+                try
+                {
+                    vabCrafts = Directory.GetFiles(savedPath + "/VAB").ToList();
+                    sphCrafts = Directory.GetFiles(savedPath + "/SPH").ToList();
+                }
+                catch
+                {
+                    vabCrafts = new List<string>();
+                    sphCrafts = new List<string>();
+                }
+                if(vabCrafts.Count()>0)
+                {
+                    for(int i = 0; i<vabCrafts.Count(); i++)
+                    {
+                        GUILayout.Label("Name: " + vabCrafts.ElementAt(i));
+                        if (GUILayout.Button("Delete"))
+                        {
+                            File.Delete(vabCrafts.ElementAt(i));
+                            alwaysSave.Remove(vabCrafts.ElementAt(i));
+                        }
+                    }
+                }
+                if (sphCrafts.Count() > 0)
+                {
+                    for (int i = 0; i < sphCrafts.Count(); i++)
+                    {
+                        GUILayout.Label("Name: " + sphCrafts.ElementAt(i));
+                        if (GUILayout.Button("Delete"))
+                        {
+                            File.Delete(sphCrafts.ElementAt(i));
+                            alwaysSave.Remove(sphCrafts.ElementAt(i));
+                        }
+                    }
+                }
+                GUILayout.EndScrollView();
+            }
+            if (GUILayout.Button("Save Craft"))
+            {
+                confirmed = false;
+                string sourcePath;
+                if (EditorLogic.fetch.ship.shipFacility == EditorFacility.VAB) sourcePath = VABPath + EditorLogic.fetch.ship.shipName + ".craft";
+                else sourcePath = SPHPath + EditorLogic.fetch.ship.shipName + ".craft";
+                string destPath;
+                if (EditorLogic.fetch.ship.shipFacility == EditorFacility.VAB) destPath = savedPath + "/VAB/" + EditorLogic.fetch.ship.shipName + ".craft";
+                else destPath = savedPath + "/SPH/" + EditorLogic.fetch.ship.shipName + ".craft";
+                Directory.CreateDirectory(savedPath + "/VAB");
+                Directory.CreateDirectory(savedPath + "/SPH");
+                if(!File.Exists(sourcePath))
+                {
+                    promptToSave = true;
+                    return;
+                }
+                if (!File.Exists(destPath))
+                {
+                    SaveCraft();
+                }
+                else askToConfirm = true;
+            }
+            alwaysSaveToggle = GUILayout.Toggle(alwaysSaveToggle, "Save all future versions of this craft");
+            if(askToConfirm)
+            {
+                GUILayout.Label("Craft File Already Exists, do you want to overwrite?");
+                if(GUILayout.Button("Yes"))
+                {
+                    SaveCraft();
+                }
+                if(GUILayout.Button("No"))
+                {
+                    askToConfirm = false;
+                }
+            }
+            else if(confirmed)
+            {
+                GUILayout.Label("Craft saved successfully");
+            }
+            if(promptToSave)
+            {
+                GUILayout.Label("Save your craft first");
+            }
+            
+            GUI.DragWindow();
+        }
+        void SaveCraft()
+        {
+            string sourcePath;
+            if (EditorLogic.fetch.ship.shipFacility == EditorFacility.VAB) sourcePath = VABPath + EditorLogic.fetch.ship.shipName + ".craft";
+            else sourcePath = SPHPath + EditorLogic.fetch.ship.shipName + ".craft";
+            string destPath;
+            if (EditorLogic.fetch.ship.shipFacility == EditorFacility.VAB) destPath = savedPath + "/VAB/" + EditorLogic.fetch.ship.shipName + ".craft";
+            else destPath = savedPath + "/SPH/" + EditorLogic.fetch.ship.shipName + ".craft";
+            Directory.CreateDirectory(savedPath + "/VAB");
+            Directory.CreateDirectory(savedPath + "/SPH");
+            File.Copy(sourcePath, destPath, true);
+            if (alwaysSaveToggle && !alwaysSave.Contains(EditorLogic.fetch.ship.shipName + ".craft")) alwaysSave.Add(EditorLogic.fetch.ship.shipName + ".craft");
+            Debug.Log("[CraftSaver]: Saved " + EditorLogic.fetch.ship.shipName);
+            askToConfirm = false;
+            confirmed = true;
+            promptToSave = false;
+            alwaysSaveToggle = false;
+        }
+        private void OnDestroy()
+        {
+            if (ToolbarButton != null) ApplicationLauncher.Instance.RemoveModApplication(ToolbarButton);
+            showGUI = false;
+            GameEvents.onGUIApplicationLauncherReady.Remove(GUIReady);
+        }
+    }
+}
